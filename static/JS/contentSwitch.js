a2e2b2bbd8490d2491552450603207382983428b
yusufmj <yusufmj605@gmail.com>
2024-06-28 23:55:20 +0300

    updates(design): fixed some html and css for the design formatting of the page, so that login and registration ar ein the middle

--- static/JS/contentSwitch.js
+++ static/JS/contentSwitch.js
@@ -5,35 +5,101 @@ function changeContent(page) {
     switch (page) {
         case 'home':
             contentDiv.innerHTML = `
-                nothing for now`;
+                <div class="contentpost">
+                <div class="posts">  
+                    <div class="post">
+                        <div class="title">
+                            <h2>marwa</h2>
+                        </div>
+                        <div class="content">
+                            dnjencjdnjcndnckegfnbvgjnbjgb
+                        </div>
+                    </div><div class="post">
+                        <div class="title">
+                            <h2>marwa</h2>
+                        </div>
+                        <div class="content">
+                            dnjencjdnjcndnckegfnbvgjnbjgb
+                        </div>
+                    </div>
+                    <div class="post">
+                        <div class="title">
+                            <h2>marwa</h2>
+                        </div>
+                        <div class="content">
+                            dnjencjdnjcndnckegfnbvgjnbjgb
+                        </div>
+                    </div><div class="post">
+                        <div class="title">
+                            <h2>marwa</h2>
+                        </div>
+                        <div class="content">
+                            dnjencjdnjcndnckegfnbvgjnbjgb
+                        </div>
+                    </div><div class="post">
+                        <div class="title">
+                            <h2>marwa</h2>
+                        </div>
+                        <div class="content">
+                            dnjencjdnjcndnckegfnbvgjnbjgb
+                        </div>
+                    </div><div class="post">
+                        <div class="title">
+                            <h2>marwa</h2>
+                        </div>
+                        <div class="content">
+                            dnjencjdnjcndnckegfnbvgjnbjgb
+                        </div>
+                    </div><div class="post">
+                        <div class="title">
+                            <h2>marwa</h2>
+                        </div>
+                        <div class="content">
+                            dnjencjdnjcndnckegfnbvgjnbjgb
+                        </div>
+                    </div><div class="post">
+                        <div class="title">
+                            <h2>marwa</h2>
+                        </div>
+                        <div class="content">
+                            dnjencjdnjcndnckegfnbvgjnbjgb
+                        </div>
+                    </div>
+                </div>
+        
+            </div>`;
             break;
         case 'signup':
             contentDiv.innerHTML = `
-            <form action="/signup" method="post">
-                <label for="em">Email</label>
-                <input type="email" name="em" id="em">
-                <br>
-                <label for="us">Username</label>
-                <input type="text" name="us" id="us">
-                <br>
-                <label for="password">password</label>
-                <input type="ps" name="ps" id="ps"><br>
-                <button type="submit">SignUp</button> 
-                <p>You have an account? click <a href="#" onclick="changeContent('login')"> here</a> to login!
-                </p>
+            <form class="formAuth" action="/signup" method="post">
+                <div>
+                    <label for="em">Email</label>
+                    <input type="email" name="em" id="em">
+                    <br>
+                    <label for="us">Username</label>
+                    <input type="text" name="us" id="us">
+                    <br>
+                    <label for="password">password</label>
+                    <input type="ps" name="ps" id="ps"><br>
+                    <button type="submit">SignUp</button> 
+                    <p>You have an account? click <a href="#" onclick="changeContent('login')"> here</a> to login!
+                    </p>
+                </div>
             </form>`;
             break;
         case 'login':
-            contentDiv.innerHTML = 
-                `<form action="/login" method="post">
-                <label for="em">Email</label>
-                <input type="email" name="em" id="em">
-                <br>
-                <label for="password">password</label>
-                <input type="ps" name="ps" id="ps"><br>
-                <button type="submit">Login</button> 
-                <p>don't have an account? click <a href="#" onclick="changeContent('signup')"> here!</a>
-                </p>
+            contentDiv.innerHTML = `
+            <form class="formAuth" action="/login" method="post">
+                <div>
+                    <label for="em">Email</label>
+                    <input type="email" name="em" id="em">
+                    <br>
+                    <label for="password">password</label>
+                    <input type="ps" name="ps" id="ps"><br>
+                    <button type="submit">Login</button> 
+                    <p>don't have an account? click <a href="#" onclick="changeContent('signup')"> here!</a>
+                    </p>
+                </div>
             </form>`;
             break;
  
